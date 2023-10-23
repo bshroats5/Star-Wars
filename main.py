ad7a92e4cee7a0c6c090ce742d9f0968417dcee8
Bret Shroats <81059443+bshroats5@users.noreply.github.com>
2023-10-23 11:45:30 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+star_wars_movies = [
+    ["The Phantom Menace", "Attack of the Clones", "Revenge of the Sith"],
+    ["A New Hope", "The Empire Strikes Back", "Return of the Jedi"],
+    ["The Force Awakens", "The Last Jedi", "The Rise of Skywalker"],
+]
+
+# Ask the user for the number of the trilogy
+trilogy_number = int(input("Enter the number of the trilogy (1, 2 or 3): "))
+
+# Ask the user for the number of the film in the trilogy
+film_number = int(input("Enter the number of the film in the trilogy (1, 2 or 3): "))
+
+# Print the title of the film corresponding to the user selection
+film_title = star_wars_movies[trilogy_number - 1][film_number - 1]
+print("The title of the film is:", film_title)
